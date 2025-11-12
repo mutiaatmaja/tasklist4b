0afaba85497f98ea3210eadfb66fd105169338a7
mutiaatmaja <mutiaatmaja80@guru.smk.belajar.id>
2025-11-12 08:43:23 +0700

    init

---
+++ resources/views/components/⚡agenda.blade.php
@@ -0,0 +1,19 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component {
+    //
+};
+?>
+
+<div>
+    <h3>
+        Hallo !!!, Silahkan Catat Kegiatan Anda
+    </h3>
+    <hr color="red" />
+    ini tempat untuk menambah/edit kegiatan
+    <hr color="blue" />
+    ini untuk tampilan table kegiatan anda
+    <hr color="black" />
+</div>
