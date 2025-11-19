4b14a893aa8956decb6a7dbfab33642a194c5e91
mutiaatmaja <mutiaatmaja80@guru.smk.belajar.id>
2025-11-19 07:47:36 +0700

    form

--- resources/views/components/⚡agenda.blade.php
+++ resources/views/components/⚡agenda.blade.php
@@ -12,7 +12,24 @@ new class extends Component {
         Hallo !!!, Silahkan Catat Kegiatan Anda
     </h3>
     <hr color="red" />
-    ini tempat untuk menambah/edit kegiatan
+    <form>
+        <label>Nama Kegiatan</label>
+        <input type="text" wire:model="nama_kegiatan">
+        <label>Status</label>
+        <select wire:model="status">
+            <option value="Baru">Baru</option>
+            <option value="Pending">Pending</option>
+            <option value="Ngambang">Ngambang</option>
+            <option value="Bambang">Bambang</option>
+            <option value="Done">Done</option>
+        </select>
+        <label>Mood</label>
+        <select wire:model="mood">
+            <option value="Happy">Happy</option>
+            <option value="Angry">Angry</option>
+        </select>
+    </form>
+
     <hr color="blue" />
     ini untuk tampilan table kegiatan anda
     <hr color="black" />
